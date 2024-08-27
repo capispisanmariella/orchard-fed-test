81a0128a90c1299ea2a4ce55ffc9b08afc7a8308
Mariella Capispisan <44761845+capispisanmariella@users.noreply.github.com>
2024-08-27 15:42:34 +0000

    Adding webpack as build tool

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+import '../scss/styles.scss';
+import img1 from '../assets/component-01/Image-01.jpg';
+import img2 from '../assets/component-01/Image-02.jpg';
+import img3 from '../assets/component-01/Image-03.jpg';
+import img4 from '../assets/component-02/Image-01.jpg';
+import img5 from '../assets/component-02/Image-02.jpg';
+import img6 from '../assets/component-02/Image-03.jpg';
+
 document.addEventListener('DOMContentLoaded', function() {
     // Capture all anchor clicks
     document.querySelectorAll('a').forEach(anchor => {
@@ -12,11 +20,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const modalImg = document.getElementById('modal-img');
     const closeBtn = document.querySelector('.close');
 
-    document.querySelectorAll('.modal-image').forEach(img => {
+    document.querySelectorAll('.modal-image').forEach((img, index) => {
+        img.src = [img1, img2, img3][index];
         img.addEventListener('click', function() {
             modal.style.display = 'flex';
             modalImg.src = this.src;
         });
+    });
+
+    document.querySelectorAll('.aeg-img').forEach((img, index) => {
+        img.src = [img4, img5, img6][index];
     });
     
     closeBtn.onclick = function() {
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './js/script.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+    assetModuleFilename: 'assets/[name].[hash][ext]', 
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.css'
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+};
